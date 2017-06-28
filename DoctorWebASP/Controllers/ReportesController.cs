b44bc65909d7a86c3d0acae2eba17cbeb1d964d2
Atahualpa Silva F. <atahualpasilva@gmail.com>
2017-06-28 17:20:59 -0400

    Excepciones de los reportes prestablecidos.
    Falta acomodar la vista para el manejo de errores.

--- DoctorWebASP/Controllers/ReportesController.cs
+++ DoctorWebASP/Controllers/ReportesController.cs
@@ -32,6 +32,7 @@ namespace DoctorWebASP.Controllers
             db.Dispose();
         }
 
+        #region REPORTES PRESTABLECIDOS
         // GET: Reportes
         public ActionResult Index()
         {
@@ -89,29 +90,42 @@ namespace DoctorWebASP.Controllers
             return View(indexViewModel);
         }
 
-        public ActionResult Configurados()
-        {
-            IEnumerable<string> result = getEntities();
-
-            return View(result);
-        }
-
         #region REPORTE #1 - Cantidad de usuarios registrados en un tiempo determinado
         [HttpPost]
         public ActionResult getCantidadUsuariosRegistrados(string fechaInicioStr, string fechaFinStr)
         {
-            DateTime fechaInicio = DateTime.Parse(fechaInicioStr + " " + firstTimeOnDay, CultureInfo.InvariantCulture);
-            DateTime fechaFin = DateTime.Parse(fechaFinStr + " " + lastTimeOnDay, CultureInfo.InvariantCulture);
+            ResultadoProceso resultadoProceso = new ResultadoProceso();
+            try
+            {
+                DateTime fechaInicio = DateTime.Parse(fechaInicioStr + " " + firstTimeOnDay, CultureInfo.InvariantCulture);
+                DateTime fechaFin = DateTime.Parse(fechaFinStr + " " + lastTimeOnDay, CultureInfo.InvariantCulture);
 
-            var result = from p in db.Personas
-                         where p.FechaCreacion >= fechaInicio & p.FechaCreacion <= fechaFin
-                         select p;
+                var result = from p in db.Personas
+                             where p.FechaCreacion >= fechaInicio & p.FechaCreacion <= fechaFin
+                             select p;
 
-            return Json(new { cantidad = result.Count(), fechaInicio = fechaInicio.ToString(), fechaFin = fechaFin.ToString() });
+                if (result == null)
+                    throw new DoctorWebException("Hay un problema con la consulta en la base de datos.");
+
+                resultadoProceso.Inicializar(result.Count().ToString());
+
+                return Json( new { resultadoProceso });
+            }
+            catch (Exception ex)
+            {
+                if (ex is SqlException || ex is DataException || ex is EntityException)
+                    resultadoProceso.Mensaje = "Hay un error de conexión con la base de datos.";
+                if (ex is FormatException)
+                    resultadoProceso.Mensaje = "Hay un error de con el formato de las fechas.";
+                else
+                    resultadoProceso.Mensaje = (ex.InnerException == null) ? ex.Message : ex.InnerException.Message;
+
+                return Json(new { resultadoProceso });
+            }
         }
         #endregion
 
-        #region REPORTE #2 - Promedio de edad de los pacientes.
+        #region CHECKHED ***** REPORTE #2 - Promedio de edad de los pacientes.
         public double getPromedioEdadPaciente()
         {
             var result = from p in db.Personas
@@ -143,7 +157,7 @@ namespace DoctorWebASP.Controllers
         }
         #endregion
 
-        #region REPORTE #3 - Promedio de citas por médico.
+        #region CHECKHED ***** REPORTE #3 - Promedio de citas por médico.
         public double getPromedioCitasPorMedico()
         {
             double? cantidadCitas = (from c in db.Calendarios
@@ -218,31 +232,24 @@ namespace DoctorWebASP.Controllers
                     throw new NotFiniteNumberException("La operación retornó un número no válido.");
 
                 resultadoProceso.Inicializar(promedio.ToString());
-                //return Json(new { cantidad = totalCantidadRecursos / cantidadRecursos});
-                //return (Newtonsoft.Json.JsonConvert.SerializeObject(ResultadoProceso));
-                //return JsonConvert.SerializeObject(ResultadoProceso, Formatting.Indented);
+
                 return Json(new { resultadoProceso } );
-                //return Json(new { metricas = metrics });
-            }
-            catch (FormatException)
-            {
-                resultadoProceso.Mensaje = "Hay un error con el formato de fecha.";
-                return Json(new { resultadoProceso });
-            }
-            catch (SqlException)
-            {
-                resultadoProceso.Mensaje = "Hay un error de conexión con la base de datos.";
-                return Json(new { resultadoProceso });
-            }
+            }            
             catch(Exception ex)
             {
-                resultadoProceso.Mensaje = (ex.InnerException == null) ? ex.Message : ex.InnerException.Message;
+                if (ex is SqlException || ex is DataException || ex is EntityException)
+                    resultadoProceso.Mensaje = "Hay un error de conexión con la base de datos.";
+                if (ex is FormatException)
+                    resultadoProceso.Mensaje = "Hay un error de con el formato de las fechas.";
+                else
+                    resultadoProceso.Mensaje = (ex.InnerException == null) ? ex.Message : ex.InnerException.Message;
+
                 return Json(new { resultadoProceso });
             }
         }
         #endregion
 
-        #region REPORTE #5 - Promedio de uso de la aplicación
+        #region CHECKHED ***** REPORTE #5 - Promedio de uso de la aplicación
         public double getPromedioUsoApp()
         {
             double? bitacora = (from b in db.Bitacoras
@@ -270,34 +277,66 @@ namespace DoctorWebASP.Controllers
         [HttpPost]
         public ActionResult getPromedioCitasCanceladasPorMedico(string fechaInicioStr, string fechaFinStr)
         {
-            DateTime dtFechaInicio = DateTime.Parse(fechaInicioStr, CultureInfo.InvariantCulture);
-            DateTime dtFechaFin = DateTime.Parse(fechaFinStr, CultureInfo.InvariantCulture);
+            var resultadoProceso = new ResultadoProceso();
 
-            double? cantidadCitasCanceladas = (from c in db.Calendarios
-                                              where c.Cancelada & c.Disponible == 1 & c.HoraInicio >= dtFechaInicio & c.HoraFin <= dtFechaFin
-                                              select c).Count();
-            double? cantidadMedicos = (from p in db.Personas
-                                      where p is Medico
-                                      select p).Count();
+            try
+            {
+                DateTime dtFechaInicio = DateTime.Parse(fechaInicioStr, CultureInfo.InvariantCulture);
+                DateTime dtFechaFin = DateTime.Parse(fechaFinStr, CultureInfo.InvariantCulture);
 
-            if (cantidadCitasCanceladas == null || cantidadMedicos == null)
-                throw new Exception("Hay un problema con la consulta en la base de datos.");
+                double? cantidadCitasCanceladas = (from c in db.Calendarios
+                                                  where c.Cancelada & c.Disponible == 1 & c.HoraInicio >= dtFechaInicio & c.HoraFin <= dtFechaFin
+                                                  select c).Count();
+                double? cantidadMedicos = (from p in db.Personas
+                                          where p is Medico
+                                          select p).Count();
 
-            if (cantidadMedicos == 0)
-                throw new DivideByZeroException("Hay un error de división entre cero.");
+                if (cantidadCitasCanceladas == null || cantidadMedicos == null)
+                    throw new Exception("Hay un problema con la consulta en la base de datos.");
 
-            return Json(new { cantidad = cantidadCitasCanceladas / cantidadMedicos, fechaInicio = dtFechaInicio.ToString(), fechaFin = dtFechaFin.ToString() });
+                if (cantidadMedicos == 0)
+                    throw new DivideByZeroException("Hay un error de división entre cero.");
+
+                double promedio = (double)cantidadCitasCanceladas / (double)cantidadMedicos;
+
+                if (Double.IsInfinity(promedio) || Double.IsNaN(promedio))
+                    throw new NotFiniteNumberException("La operación retornó un número no válido.");
+
+                resultadoProceso.Inicializar(promedio.ToString());
+
+                return Json(new { resultadoProceso });
+            }
+            catch (Exception ex)
+            {
+                if (ex is SqlException || ex is DataException || ex is EntityException)
+                    resultadoProceso.Mensaje = "Hay un error de conexión con la base de datos.";
+                if (ex is FormatException)
+                    resultadoProceso.Mensaje = "Hay un error de con el formato de las fechas.";
+                else
+                    resultadoProceso.Mensaje = (ex.InnerException == null) ? ex.Message : ex.InnerException.Message;
+
+                return Json(new { resultadoProceso });
+            }
         }
         #endregion
+        #endregion
+
+        #region REPORTES CONFIGURADOS
+        public ActionResult Configurados()
+        {
+            IEnumerable<string> result = getEntities();
+
+            return View(result);
+        }
 
         public IEnumerable<string> getEntities()
         {
             List<string> entities = new List<string>();
 
-            entities.Add("Paciente");
-            entities.Add("Medico");
-            entities.Add("Recurso Hospitalario");
             entities.Add("Centro Medico");
+            entities.Add("Medico");
+            entities.Add("Paciente");
+            entities.Add("Recurso Hospitalario");
 
             return entities;
         }
@@ -313,13 +352,13 @@ namespace DoctorWebASP.Controllers
 
             foreach (var se in selectedEntities)
             {
-                if (se.Equals("Paciente"))
+                if (se.Equals("Centro Medico"))
                 {
-                    entity = new Paciente();
+                    entity = new CentroMedico();
 
                     foreach (PropertyInfo prop in entity.GetType().GetProperties())
                     {
-                        if (prop.Name.Equals("Nombre") || prop.Name.Equals("Apellido") || prop.Name.Equals("TipoSangre"))
+                        if (prop.Name.Equals("Nombre"))
                             attributes.Add(se + "." + prop.Name);
                     }
                 }
@@ -335,6 +374,17 @@ namespace DoctorWebASP.Controllers
                     }
                 }
 
+                if (se.Equals("Paciente"))
+                {
+                    entity = new Paciente();
+
+                    foreach (PropertyInfo prop in entity.GetType().GetProperties())
+                    {
+                        if (prop.Name.Equals("Nombre") || prop.Name.Equals("Apellido") || prop.Name.Equals("TipoSangre"))
+                            attributes.Add(se + "." + prop.Name);
+                    }
+                }
+
                 if (se.Equals("Recurso Hospitalario"))
                 {
                     entity = new RecursoHospitalario();
@@ -342,17 +392,6 @@ namespace DoctorWebASP.Controllers
                     foreach (PropertyInfo prop in entity.GetType().GetProperties())
                     {
                         if (prop.Name.Equals("Nombre") || prop.Name.Equals("Tipo"))
-                            attributes.Add(se + "." + prop.Name);
-                    }
-                }
-
-                if (se.Equals("Centro Medico"))
-                {
-                    entity = new CentroMedico();
-
-                    foreach (PropertyInfo prop in entity.GetType().GetProperties())
-                    {
-                        if (prop.Name.Equals("Nombre"))
                             attributes.Add(se + "." + prop.Name);
                     }
                 }
@@ -366,19 +405,44 @@ namespace DoctorWebASP.Controllers
         {
             List<string> metrics = new List<string>();
 
-            if (selectedEntities == null || selectedAttributes == null)
-                throw new Exception("Hay un problema con la consulta en la base de datos.");
-
             if (selectedEntities.Count() == 2)
             {
-                if (selectedEntities.Contains("Paciente") & selectedEntities.Contains("Medico"))
+                if (selectedEntities.Contains("Medico") & selectedEntities.Contains("Paciente"))
                 {
-                    metrics.Add("Lista de pacientes por medico.");
-                    metrics.Add("Lista de medicos por paciente.");
+                    if (selectedAttributes.Contains("Medico.Nombre") & selectedAttributes.Contains("Medico.Apellido") & selectedAttributes.Contains("Paciente.Nombre") || selectedAttributes.Contains("Paciente.Apellido"))
+                    {
+                        metrics.Add("Lista de pacientes por medico.");
+                    }
                 }
             }
 
             return Json(new { metricas = metrics });
+        }
+
+        [HttpPost]
+        public string getReport(string selectedMetric)
+        {
+            dynamic query = null;
+
+            if (selectedMetric.Equals("Lista de pacientes por medico."))
+            {
+                query = from m in db.Personas
+                        where m is Medico
+                        join ca in db.Calendarios
+                        on m equals ca.Medico
+                        join ci in db.Citas
+                        on ca equals ci.Calendario
+                        join pa in db.Personas
+                        on ci.Paciente equals pa
+                        where pa is Paciente
+                        select new
+                        {
+                            Medico = m.Nombre + " " + m.Apellido,
+                            Paciente = pa.Nombre + " " + pa.Apellido
+                        };
+            }
+
+            return (Newtonsoft.Json.JsonConvert.SerializeObject(query));
         }
 
         public int pruebaunitaria()
@@ -386,5 +450,6 @@ namespace DoctorWebASP.Controllers
             var result = 2 + 2;
             return result;
         }
+        #endregion
     }
 }
