e9264f62ab8842a3dd89b590087be3ca4984d46c
eavv10 <enrique.vilagut@gmail.com>
2017-06-28 16:12:08 -0400

    acomodando interfaz login

--- DoctorWebASP/App_Start/Startup.Auth.cs
+++ DoctorWebASP/App_Start/Startup.Auth.cs
@@ -29,7 +29,8 @@ namespace DoctorWebASP
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    // This is a security feature which is used when you change a 
+                    or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
