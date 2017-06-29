95959b4257c4b2aae9cbac6f58e0d67e5b87cf90
eliterox <eliterox@gmail.com>
2017-06-28 23:40:21 -0400

    Ultimo Commit

---
+++ DoctorWebASP/Controllers/PacientesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoctorWebASP.Models;
+using Microsoft.AspNet.Identity;
+
+namespace DoctorWebASP.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            return View(db.Personas.OfType<Paciente>().ToList());
+        }
+
+        // GET: Pacientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Personas.OfType<Paciente>().Where(p => p.PersonaId == id).Single();
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pacientes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PersonaId,Nombre,Apellido,Cedula,Genero,Telefono,FechaNacimiento,FechaCreacion,Email,Direccion,TipoSangre")] Paciente paciente)
+        {
+            string userId = User.Identity.GetUserId();
+            ApplicationUser user = db.Users.Single(u => u.Id == userId);
+            if (ModelState.IsValid)
+            {
+                paciente.ApplicationUser = user;
+                db.Personas.Add(paciente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Personas.OfType<Paciente>().Where(p => p.PersonaId == id).Single();
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Pacientes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonaId,Nombre,Apellido,Cedula,Genero,Telefono,FechaNacimiento,FechaCreacion,Email,Direccion,TipoSangre")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Personas.OfType<Paciente>().Where(p => p.PersonaId == id).Single();
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Pacientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Paciente paciente = db.Personas.OfType<Paciente>().Where(p => p.PersonaId == id).Single();
+            db.Personas.Remove(paciente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
