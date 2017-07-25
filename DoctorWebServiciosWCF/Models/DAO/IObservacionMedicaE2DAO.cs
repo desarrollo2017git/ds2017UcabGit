875bbf6826329b281a7655c0decc71a383ca846a
Andrew P <lui21andrew@gmail.com>
2017-07-25 09:01:04 -0400

    Dao y Servicios

---
+++ DoctorWebServiciosWCF/Models/DAO/IObservacionMedicaE2DAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoctorWebServiciosWCF.Models.DAO
+{
+    public interface IObservacionMedicaE2DAO
+    {
+        // Metodo del DAO para guardar Observaciones Medias en la Base de datos
+        void GuardarObservacionMedicaE2(ObservacionMedicaE2 observacionMedicaE2);
+
+        // Metodo del DAO para obtener una lista de las observaciones medicas
+        List<ObservacionMedicaE2> ObtenerSelectListObservacionMedicaE2();
+
+        // Metodo del DAO utilizado para eliminar observaciones medicas. 
+        void EliminarObservacionMedicaE2(ObservacionMedicaE2 observacionMedicaE2);
+
+    }
+}
---
+++ DoctorWebServiciosWCF/Models/DAO/ObservacionMedicaE2DAO.cs
@@ -0,0 +1,74 @@
+﻿using DoctorWebServiciosWCF.Helpers;
+using DoctorWebServiciosWCF.Models.ORM;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DoctorWebServiciosWCF.Models.DAO
+{
+    using Modelo = ObservacionMedicaE2;
+    public class ObservacionMedicaE2DAO : DAO<Modelo>, IObservacionMedicaE2DAO
+    {
+        /// <summary>
+        /// Metodo del Data Access Object utilizado para eliminar observaciones. 
+        /// </summary>
+        /// <param name="observacionMedicaE2">observacion que se desea eliminar</param>
+        public void EliminarObservacionMedicaE2(Modelo observacionMedicaE2)
+        {
+            var notificacionDAO = Utilidades.Instancia.Fabrica.CrearNotificacionDAO();
+            try
+            {
+                // Obtenemos la cita a eliminar de la BD usando el comando ObtenerPrimeroQue
+                // luego eliminamos la observacion con el comando borrar
+                var observacionAEliminar = ObtenerPrimeroQue(c => c.ObservacionMedicaId == observacionMedicaE2.ObservacionMedicaId);
+                Borrar(observacionAEliminar);
+            }
+            catch (DoctorWebException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw Utilidades.Instancia.Fabrica.CrearExcepcion(interna: e);
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// Metodo del DAO para guardar observaciones en la Base de datos
+        /// </summary>
+        /// <param name="observacionMedicaE2">observacion que se desea guardar</param>
+        public void GuardarObservacionMedicaE2(Modelo observacionMedicaE2)
+        {
+
+            try
+            {
+                // Creamos la observacion utilizando comando Crear
+                Crear(observacionMedicaE2);
+            }
+            catch (DoctorWebException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw Utilidades.Instancia.Fabrica.CrearExcepcion(interna: e);
+            }
+        }
+
+        /// <summary>
+        /// Metodo del DAO para obtener una lista de las observaciones medicas
+        /// </summary>
+        /// <returns>Lista de observaciones medicas</returns>
+        public List<ObservacionMedicaE2> ObtenerSelectListObservacionMedicaE2()
+        {
+            var dao = Utilidades.Instancia.Fabrica.CrearDAO<ObservacionMedicaE2>();
+            return dao.ObtenerTodos().ToList();
+        }
+    }
+
+}
